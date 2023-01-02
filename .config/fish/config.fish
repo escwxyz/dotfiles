ce01be9193dbf2564f6a2ace779b117f09b6e575
Jie <dr.lodisy@gmail.com>
2023-01-02 13:23:35 +0800

    [fish]: add flutter path and alacritty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,12 @@
 fish_vi_key_bindings
 set -g fish_greeting
 # add path (installed before fish)
-set -gx PATH /home/jiew/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+set -gx PATH /home/jiew/alacritty/target/release/alacritty:/home/jiew/flutter/bin:/home/jiew/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 set -gx EITOR nvim
 set -gx LANG en_US.UTF-8
+# flutter mirrors
+set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn 
+set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
