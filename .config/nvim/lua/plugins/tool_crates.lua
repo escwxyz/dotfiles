38fc4bddee0382bafb64ec8f2940e332d7459b84
Jie <dr.lodisy@gmail.com>
2023-01-04 22:59:39 +0800

    [nvim]: allow conditional load for crates.nvim

--- .config/nvim/lua/plugins/tool_crates.lua
+++ .config/nvim/lua/plugins/tool_crates.lua
@@ -1,6 +1,13 @@
 return {
     "saecki/crates.nvim",
-    ft = "toml",--TODO
+    lazy = false,
+    cond = function()
+        local buf = vim.api.nvim_buf_get_name(0)
+        if string.sub(buf, -10) == "Cargo.toml" then
+            return true
+        end
+        return false
+    end,
     config = function()
         require("crates").setup()
     end,
