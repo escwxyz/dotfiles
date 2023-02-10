ac6d9cfbe019bf8f683d3bf699297e9f3ce2bb27
Michael <linsijia628@gmail.com>
2023-02-10 10:08:13 +0800

    chore(nvim): minor changes

--- nvim/lua/plugins/heirline/config.lua
+++ nvim/lua/plugins/heirline/config.lua
@@ -2,9 +2,10 @@ local M = {}
 
 M.setup = function()
     require("heirline").setup({
-        statusline = require("plugins.heirline.components.statusline"),
+        -- statusline = require("plugins.heirline.components.statusline"),
         winbar = require("plugins.heirline.components.winbar"),
-        tabline = require("plugins.heirline.components.tabline"),
+        -- tabline = require("plugins.heirline.components.tabline"),
+        tabline = require("plugins.heirline.components.statusline"),
     })
 end
 
