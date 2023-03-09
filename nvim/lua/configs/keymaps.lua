0697b8a2c1f504022453ded662b6caec80c5a485
Michael <linsijia628@gmail.com>
2023-03-09 10:29:18 +0800

    chore(nvim): plugin updates and remove vmux

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -15,24 +15,9 @@ vim.keymap.set("n", "/", "<cmd>FzfLua blines<CR>", { desc = "Search in buffer" }
 
 vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
 
--- TODO
-vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<S-Tab>", "<cmd>tabnext<CR>", { desc = "Next tabpage" })
+vim.keymap.set("n", "<C-w>", "<cmd>w<CR>", { desc = "Write buffer", silent = true })
+vim.keymap.set("i", "<C-w>", "<Esc><cmd>w<CR>", { desc = "Write buffer", silent = true })
 
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save buffer", silent = true })
-vim.keymap.set("i", "<C-s>", "<Esc><cmd>w<CR>", { desc = "Save buffer", silent = true })
--- vim.keymap.set("n", "<Tab>", function()
---     if #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
---         vim.cmd([[bnext]])
---     end
--- end, { desc = "Next buffer", expr = true })
---
--- vim.keymap.set("n", "<S-Tab>", function()
---     if #vim.api.nvim_list_tabpages() > 1 then
---         vim.cmd([[]])
---     end
--- end, { desc = "Next tabpage" })
---
 vim.keymap.set({ "n", "x", "o" }, "s", "<cmd>LeapBuffer<CR>", { desc = "[Leap] Search in buffer" })
 vim.keymap.set(
     { "n", "x", "o" },
@@ -40,9 +25,6 @@ vim.keymap.set(
     "<cmd>LeapWindow<CR>",
     { desc = "[Leap] Search cross window" }
 )
-
-vim.keymap.set("n", "<C-t>", "<cmd>FTermToggle<CR>", { desc = "Terminal" })
-vim.keymap.set("t", "<C-t>", "<C-\\><C-n><cmd>FTermToggle<CR>", { desc = "Terminal" })
 
 vim.keymap.set("n", "<leader>q", "<cmd>BufferRemove<CR>", { desc = "Quit" })
 
@@ -62,8 +44,12 @@ vim.keymap.set("n", "<leader>?", "<cmd>FzfLua help_tags<CR>", { desc = "Help tag
 vim.keymap.set("n", "<leader>/", "<cmd>FzfLua keymaps<CR>", { desc = "Keymaps" })
 vim.keymap.set("n", "<leader>\\", "<cmd>Lazy<CR>", { desc = "Plugins" })
 
+vim.keymap.set("n", "<leader>an", "<cmd>NodeAction<CR>", { desc = "Node Action" })
+
 vim.keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<CR>", { desc = "Buffers" })
 vim.keymap.set("n", "<leader>c", "<cmd>FindCommands<CR>", { desc = "Commands" })
+
+vim.keymap.set("n", "<leader>f", "<cmd>Oil --float<CR>", { desc = "Oil file manager" })
 
 vim.keymap.set("n", "<leader>h", "<cmd>FocusSplitLeft<CR>", { desc = "Split left" })
 vim.keymap.set("n", "<leader>j", "<cmd>FocusSplitDown<CR>", { desc = "Split down" })
@@ -74,7 +60,7 @@ vim.keymap.set("n", "<leader>nf", "<cmd>Neogen func<CR>", { desc = "Annotation f
 vim.keymap.set("n", "<leader>nt", "<cmd>Neogen type<CR>", { desc = "Annotation type" })
 vim.keymap.set("n", "<leader>nc", "<cmd>Neogen class<CR>", { desc = "Annotation class" })
 
-vim.keymap.set("n", "<leader>o", "<cmd>Oil --float<CR>", { desc = "Oil" })
+vim.keymap.set("n", "<leader>o", "<cmd>HydraEditorOptions<CR>", { desc = "Editor Options" })
 
 vim.keymap.set("n", "<leader>p", "<cmd>FindProjects<CR>", { desc = "Projects" })
 
@@ -82,38 +68,16 @@ vim.keymap.set("n", "<leader>q", "<cmd>BufferRemove<CR>", { desc = "Quit" })
 
 vim.keymap.set("v", "<leader>r", "<cmd>Refactor<CR>", { desc = "Refactor" })
 
--- Vmux
-vim.keymap.set("n", "<leader>s", "<cmd>VmuxSplit<CR>", { desc = "Vmux split horizontally" })
-vim.keymap.set("n", "<leader>v", "<cmd>VmuxVsplit<CR>", { desc = "Vmux split vertically" })
+vim.keymap.set("n", "<leader>s", "<cmd>FzfLua files<CR>", { desc = "Search files" })
+
 -- Escape from terminal
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
 
-vim.keymap.set("n", "<leader>t", "<cmd>HydraEditorOptions<CR>", { desc = "Toggle options" })
+vim.keymap.set("n", "<leader>t", "<cmd>FTermToggle<CR>", { desc = "Terminal" })
+vim.keymap.set("t", "<leader>t", "<C-\\><C-n><cmd>FTermToggle<CR>", { desc = "Terminal" })
 
 vim.keymap.set("n", "<leader>u", "<cmd>HydraUrlView<CR>", { desc = "Urls" })
 
 vim.keymap.set("n", "<leader>y", "<cmd>Neoclip<CR>", { desc = "Yank history" })
 
 vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Zen mode" })
-
-local ok, wk = pcall(require, "which-key")
-
-if not ok then
-    vim.notify("WhichKey is not loaded, which is required to set keymaps", vim.log.levels.ERROR)
-    return
-end
-
-wk.register({
-    a = {
-        name = "Actions",
-        n = { "<cmd>NodeAction<CR>", "Node action" },
-    },
-}, { prefix = "<leader>" })
-
-wk.register({
-    f = {
-        name = "Files",
-
-        f = { "<cmd>FzfLua files<CR>", "Find files" },
-    },
-}, { prefix = "<leader>" })
