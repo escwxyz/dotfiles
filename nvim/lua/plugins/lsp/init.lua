237fcb48d4638f4ec6ed16d0b789045c67e42a38
Michael <linsijia628@gmail.com>
2023-04-15 10:59:48 +0800

    feat(nvim): add some rust snippets

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,17 +1,3 @@
-local function get_library()
-    local lib = {
-        vim.api.nvim_get_runtime_file("", true),
-    }
-    -- TODO
-    if vim.fn.has("mac") > 0 then
-        local hammerspoon =
-            string.format("%s/.hammerspoon/Spoons/EmmyLua.spoon/annotations", os.getenv("HOME"))
-        table.insert(lib, hammerspoon)
-    end
-
-    return lib
-end
-
 local border = {
     { "╭", "FloatBorder" },
     { "─", "FloatBorder" },
@@ -26,7 +12,7 @@ local border = {
 local M = {
     "neovim/nvim-lspconfig",
     enabled = true,
-    event = "BufReadPre", -- NOTE improper event leading to unexpected behavior
+    event = "BufEnter",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         {
@@ -46,17 +32,6 @@ local M = {
                 })
             end,
         },
-        -- {
-        --     -- config = function()
-        --     --     require("lsp-inlayhints").setup({
-        --     --         inlay_hints = {
-        --     --             parameter_hints = {
-        --     --                 show = false,
-        --     --             },
-        --     --         },
-        --     --     })
-        --     -- end,
-        -- },
     },
     config = function()
         vim.lsp.handlers["textDocument/hover"] =
@@ -66,52 +41,10 @@ local M = {
             vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
 
         require("plugins.lsp.diagnostics").define_diagnostics()
-        require("lspconfig").tailwindcss.setup({
-            on_attach = require("plugins.lsp.on_attach"),
-            capabilities = require("plugins.lsp.capabilities"),
-        })
-        -- require("lspconfig").lua_ls.setup({
-        --     on_attach = require("plugins.lsp.on_attach"),
-        --     capabilities = require("plugins.lsp.capabilities"),
-        --     settings = {
-        --         Lua = {
-        --             runtime = { version = "LuaJIT" },
-        --             diagnostics = { globals = { "vim", "awesome", "client" } },
-        --             telemetry = { enable = false },
-        --             format = { enable = false },
-        --             completion = { callSnippet = "Replace" },
-        --             workspace = {
-        --                 library = get_library(),
-        --                 checkThirdParty = false,
-        --             },
-        --         },
-        --     },
-        -- })
-
-        require("lspconfig").ds_pinyin_lsp.setup({
-            init_options = {
-                db_path = vim.fn.stdpath("config") .. "/dict.db3",
-            },
-        })
-        require("lspconfig").rust_analyzer.setup({
-            on_attach = require("plugins.lsp.on_attach"),
-            capabilities = require("plugins.lsp.capabilities"),
-            settings = {
-                ["rust-analyzer"] = {
-                    checkOnSave = {
-                        command = "clippy",
-                    },
-                },
-            },
-        })
-        require("lspconfig").prismals.setup({
-            on_attach = require("plugins.lsp.on_attach"),
-            capabilities = require("plugins.lsp.capabilities"),
-        })
-        require("lspconfig").astro.setup({
-            on_attach = require("plugins.lsp.on_attach"),
-            capabilities = require("plugins.lsp.capabilities"),
-        })
+        require("plugins.lsp.servers").setup_lua()
+        require("plugins.lsp.servers").setup_rust()
+        require("plugins.lsp.servers").setup_pinyin()
+        require("plugins.lsp.servers").setup_tailwindcss()
     end,
 }
 
