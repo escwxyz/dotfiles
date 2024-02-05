62091d5b07cde38931e04b33341ecc7ecbcd505e
Jie <linsijia628@gmail.com>
2024-02-05 15:20:20 +0800

    chore: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,31 +7,30 @@ switch (uname -s)
         abbr --add --global z zellij
 
         set -gx HOME /Users/jiewang
+        set CARGO "$HOME/.cargo/bin"
         set SQLITE /opt/homebrew/opt/sqlite/bin
         set SOLANA "$HOME/solana/bin"
         set HOMEBREW /opt/homebrew/bin
+        set -gx PNPM_HOME /Users/jiewang/Library/pnpm
+        if not string match -q -- $PNPM_HOME $PATH
+            set -gx PATH "$PNPM_HOME" $PATH
+        end
         set SYSTEM "/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/sbin:/Library/Apple/usr/bin:/usr/local/go/bin"
         set WEZTERM "/Applications/WezTerm.app/Contents/MacOS"
         set NEOVIDE "/Applications/Neovide.app/Contents/MacOS"
-        set NPM "$PNPM/nodejs/18.12.1/bin"
+        set NPM "$PNPM_HOME/npm"
         set POSTGRESQL /Library/PostgreSQL/14/bin/
         set ZIG "$HOME/Downloads/zig"
-        set NEXTEST "$HOME/Development/cargo-nextest"
         set FLUTTER "$HOME/Development/flutter/bin"
         set NEOVIM "$HOME/Development/nvim-macos/bin"
-        set -gx PATH "$NEXTEST:$NEOVIM:$FLUTTER:$ZIG:$POSTGRESQL:$NPM:$HOMEBREW:$CARGO:$SOLANA:$PNPM:$SQLITE:$WEZTERM:$NEOVIDE:$SYSTEM"
+        set -gx PATH "$NEOVIM:$FLUTTER:$ZIG:$POSTGRESQL:$NPM:$HOMEBREW:$CARGO:$SOLANA:$PNPM_HOME:$SQLITE:$WEZTERM:$NEOVIDE:$SYSTEM"
 
         set -gx LDFLAGS -L/opt/homebrew/opt/sqlite/lib
         set -gx CPPFLAGS -I/opt/homebrew/opt/sqlite/include
         set -gx PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
 
-        set -gx PUB_HOSTED_URL "https://pub.flutter-io.cn"
-        set -gx FLUTTER_STORAGE_BASE_URL "https://storage.flutter-io.cn"
-
-        set -gx PNPM_HOME /Users/jiewang/Library/pnpm
-        if not string match -q -- $PNPM_HOME $PATH
-            set -gx PATH "$PNPM_HOME" $PATH
-        end
+        # set -gx PUB_HOSTED_URL "https://pub.flutter-io.cn"
+        # set -gx FLUTTER_STORAGE_BASE_URL "https://storage.flutter-io.cn"
 
         # NOTE test neovim config
         # set -gx NVIM_APPNAME "neovim"
@@ -39,6 +38,8 @@ switch (uname -s)
     case Linux
 
         set -gx HOME /home/jiewang
+        set CARGO "$HOME/.cargo/bin"
+
         # TODO
 
     case '*'
@@ -52,11 +53,10 @@ set -gx SHELL fish
 set -gx EDITOR nvim
 set -gx LANG en_US.UTF-8
 
-abbr --add --global proxy "set -gx HTTP_PROXY socks5://127.0.0.1:1080 && set -gx HTTPS_PROXY socks5://127.0.0.1:1080 && cd && cd Downloads/naive && ./naive"
-abbr --add --global unproxy "set -e HTTP_PROXY && set -e HTTPS_PROXY"
-set CARGO "$HOME/.cargo/bin"
+set -gx HTTP_PROXY socks5://127.0.0.1:1080
+set -gx HTTPS_PROXY socks5://127.0.0.1:1080
 
-
+abbr --add --global proxy "cd && cd Downloads/naive && ./naive"
 
 if status is-interactive
     # eval (zellij setup --generate-auto-start fish | string collect)
