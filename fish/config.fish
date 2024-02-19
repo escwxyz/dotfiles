4b0cdc9bb3d065aa7f7eaabdcb50664a90784569
Jie <linsijia628@gmail.com>
2024-02-19 19:21:35 +0800

    chore: fish config clean up

--- fish/config.fish
+++ fish/config.fish
@@ -6,42 +6,24 @@ switch (uname -s)
     case Darwin
         abbr --add --global z zellij
 
-        set -gx HOME /Users/jiewang
-        set CARGO "$HOME/.cargo/bin"
-        set SQLITE /opt/homebrew/opt/sqlite/bin
-        set SOLANA "$HOME/solana/bin"
-        set HOMEBREW /opt/homebrew/bin
-        set -gx PNPM_HOME /Users/jiewang/Library/pnpm
-        if not string match -q -- $PNPM_HOME $PATH
-            set -gx PATH "$PNPM_HOME" $PATH
-        end
-        set SYSTEM "/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/Library/Apple/usr/bin:/usr/local/go/bin"
-        set WEZTERM "/Applications/WezTerm.app/Contents/MacOS"
-        set NEOVIDE "/Applications/Neovide.app/Contents/MacOS"
-        set NPM "$PNPM_HOME/npm"
-        set POSTGRESQL /Library/PostgreSQL/14/bin/
-        # set ZIG "$HOME/Downloads/zig"
-        set FLUTTER "$HOME/Development/flutter/bin"
-        set NEOVIM "$HOME/Development/nvim-macos/bin"
-        set NAIVE "$HOME/Downloads/naive"
-        set -gx PATH "$NAIVE:$NEOVIM:$FLUTTER:$POSTGRESQL:$NPM:$HOMEBREW:$CARGO:$SOLANA:$PNPM_HOME:$SQLITE:$WEZTERM:$NEOVIDE:$SYSTEM"
-
-        set -gx LDFLAGS -L/opt/homebrew/opt/sqlite/lib
-        set -gx CPPFLAGS -I/opt/homebrew/opt/sqlite/include
-        set -gx PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
-
+        fish_add_path /opt/homebrew/bin
+        fish_add_path ~/Development/nvim-macos/bin
+        fish_add_path ~/Development/flutter/bin
+        fish_add_path ~/Downloads/naive
+        fish_add_path ~/Library/pnpm
+        fish_add_path ~/Library/PostgreSQL/14/bin
 
         # set -gx PUB_HOSTED_URL "https://pub.flutter-io.cn"
         # set -gx FLUTTER_STORAGE_BASE_URL "https://storage.flutter-io.cn"
 
     case Linux
 
-        set -gx HOME /home/jiewang
-        set CARGO "$HOME/.cargo/bin"
-
+        # set -gx HOME /home/jiewang
+        # set CARGO "$HOME/.cargo/bin"
         # TODO
 
     case '*'
+        echo "Unspported platform!"
         # TODO
 end
 
