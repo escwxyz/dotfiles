3cc7ced87b6fe391401bf15c8ab87566fc926f8e
Jie <linsijia628@gmail.com>
2024-02-20 10:03:00 +0800

    feat: add zoxide

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,12 @@
 fish_vi_key_bindings
 
-set -l os (uname)
-
 switch (uname -s)
     case Darwin
-        abbr --add --global z zellij
-
         fish_add_path /opt/homebrew/bin
-        fish_add_path ~/Development/nvim-macos/bin
         fish_add_path ~/Development/flutter/bin
-        fish_add_path ~/Downloads/naive
         fish_add_path ~/Library/pnpm
         fish_add_path ~/Library/PostgreSQL/14/bin
+        fish_add_path ~/Development/nvim-macos/bin
 
         # set -gx PUB_HOSTED_URL "https://pub.flutter-io.cn"
         # set -gx FLUTTER_STORAGE_BASE_URL "https://storage.flutter-io.cn"
@@ -35,12 +30,17 @@ set -gx EDITOR nvim
 set -gx TERM alacritty
 set -gx LANG en_US.UTF-8
 
+fish_add_path ~/Downloads/naive
+
 set -gx ALL_PROXY socks5://127.0.0.1:1080
 set -gx HTTP_PROXY socks5://127.0.0.1:1080
 set -gx HTTPS_PROXY socks5://127.0.0.1:1080
 set -gx NO_PROXY localhost,127.0.0.1,::1
 
 alias proxy="naive ~/Downloads/naive/config.json"
+
+# zoxide
+zoxide init --cmd cd fish | source
 
 if status is-interactive
     # eval (zellij setup --generate-auto-start fish | string collect)
